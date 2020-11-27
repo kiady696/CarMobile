8967f34e8d165560574f6cfc625758a937c4c272
kix696 <Kiadyrajwels.dark@gmail.com>
2020-11-27 22:26:39 +0200

    resolving callback hell of mine

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -24,67 +24,44 @@ export class AuthenticationService {
   ){
     this.platform.ready().then( () => {
     //Check idUSer then token , else , log in
+
+    //TEST
+    this.storage.set('idUser', 'U1');
+    this.storage.set('token', 'KKKKKKKKKKKycvgubi666');
+
+
     this.ifLoggedIn();
     });
   }
 
+    //NY FONCTION IDIRANA FAHATELO
     checkToken(tokenBase){
       // MI-VERIFIER TOKEN HOE MITOVY VE 
-      this.storage.get('token').then( (val) => {// TSY MAMOAKA ERREUR ITO FA MAMOAKA EXCEPTION TYPEERROR MILA CATCHENA raha tsy mbola misy 'key' value 'token' value 
-      //onfullfilled ny ato
-      // TESTS FOTSINY LOA
-      /* if(this.checkToken(localToken)){ //raha mitovy @ any amin'ilay base de donnée ilay token local
-
-      } */
-
-      //this.authState.next(false);
-      console.log('le token local est: '+val);
-      var tokenVrai = 'kiady';
-      if(val != tokenVrai){
-        this.authState.next(false);
-
-
-      }else{
-        this.authState.next(true);
-      }
-
-      return false;
-      
-
-    
-      }, function error(localToken){
-        console.log('erreur getting local token in sqlite storage');
-        
-
-      }).catch(
-        // Promesse rejetée
-        function() { 
-          console.log("getting local token throwed an Exception ");
-        });
-        
-
-        //  if(this.checkToken(token)){
-
-        // }  
-        return false;
-
+      console.log(tokenBase.data.token);
+      console.log(tokenBase.data.expirationtoken);
+      var dateTokenBase = new Date(tokenBase.data.expirationtoken);
+      var isVrai = true;
+      this.storage.get('token').then( 
+        (token) => {
+          console.log('le token stocké etait: '+token);
+          if(token == '' || token == null){
+            isVrai = false;
+          }else{
+            /* else if(token !== tokenBase.data.token){
+            isVrai = false;
+          } *//* else if(new Date().getTime() > dateTokenBase.getTime()){
+            isVrai = false;
+          } */
+            isVrai = true;
+          }
+        }
+      )
+      return isVrai;
     } 
 
-    getToken(idUser){  
-      let token = null;
-      this.httpService.callService('Utilisateur/token/id/'+idUser).subscribe((data) => {
-      console.log(data);
-      token = data;
-      //hehe
-      });
-    return token;
-    }
-
+    //NY FONCTION IDIRANA VOALOHANY INDRINDRA  : MISY CALLBACK HELL BEEEE TSSSS
     ifLoggedIn(){
-      this.storage.ready().then(
-        () => { 
-          //onfullfilled ny ato 
-
+      
           //MI-VERIFIER idUser RAHA EFA MISY VE 
           this.storage.get('idUser').then(
             (id) => {
@@ -93,26 +70,25 @@ export class AuthenticationService {
                 this.authState.next(false);
                 return;
               }else{ // raha efa nisy idUser tao de verifiena ndray ny token 
-                let tokenBase = this.getToken(id);
-                console.log(tokenBase.tokenString);
-                if(this.checkToken(tokenBase)){ // raha ni-existe , nitovy t@ tokenBase sy tsy mbola lany date ilay token local tao
-                  this.authState.next(true);
-                }else{
-                  this.authState.next(false);
-                }   
+                this.httpService.callService('Utilisateur/token/id/'+id).subscribe((data) => {
+                  console.log(data);
+                  if(this.checkToken(data)){ // raha ni-existe , nitovy t@ tokenBase sy tsy mbola lany date ilay token local tao
+                    this.authState.next(true);
+                  }else{
+                    this.authState.next(false);
+                  }   
+                });
+                
               }
             } , function error (id){
               console.log('L\'id stocké était : '+ id);
             }
-          )
-        }
-      ).catch(
-        // Promesse rejetée
-        function() { 
-          console.log("Storage not ready (Promise in local storage readyness)");
-        });
+          ).catch((reason) => {
+            console.log(reason);
+          })
+        
 
-  }
+    }
 
 
 
